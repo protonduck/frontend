a4c720546a3509ecb3d37c31429e4385058aa8f1
Alexander Schilling <dignityinside@protonmail.com>
2022-04-23 20:55:43 +0200

    Remove jquery and other not used packages

--- src/main.js
+++ src/main.js
@@ -8,19 +8,7 @@ import { router } from './router';
 import { i18n } from './lang/i18n-setup';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import '@fortawesome/fontawesome-free/css/all.css';
-// eslint-disable-next-line import/no-extraneous-dependencies,import/extensions
-import '@fortawesome/fontawesome-free/js/all.js';
-import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-try {
-  // eslint-disable-next-line global-require,no-multi-assign
-  window.$ = window.jQuery = require('jquery');
-  // eslint-disable-next-line global-require
-  require('bootstrap');
-} catch (e) {
-  // nothing to do
-}
 
 window.Vue = require('vue');
 
