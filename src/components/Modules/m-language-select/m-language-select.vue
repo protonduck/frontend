8a0435881c2b9f8e7553f1f53c8257cbeef81cf1
Alexander Schilling <dignityinside@protonmail.com>
2022-12-04 17:07:59 +0100

    Store selected language in localstorage

--- src/components/Modules/m-language-select/m-language-select.vue
+++ src/components/Modules/m-language-select/m-language-select.vue
@@ -1,13 +1,16 @@
 <script setup>
   import i18n from '@i18n';
-  
-  // set initial language
-  languageChanged();
 
   // update on change language
   function languageChanged() {
+    localStorage.setItem('locale', i18n.global.locale);
     document.querySelector('html').setAttribute('lang', i18n.global.locale);
     document.title = i18n.global.t('site.name')
+  }
+
+  // load selected language from localStorage
+  if (localStorage.getItem('locale')) {
+    i18n.global.locale = localStorage.getItem('locale');
   }
 </script>
 
