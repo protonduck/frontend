ac7c1da13914f21b4d0f8c95f36d4de89c7661e5
Alexander Schilling <dignityinside@protonmail.com>
2022-12-04 23:52:31 +0100

    Remove superfluous aliases

--- vite.config.js
+++ vite.config.js
@@ -8,8 +8,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '@i18n': fileURLToPath(new URL('./src/i18n', import.meta.url)),
-      '@blocks': fileURLToPath(new URL('./src/components/Blocks', import.meta.url)),
       '@elements': fileURLToPath(new URL('./src/components/Elements', import.meta.url)),
       '@modules': fileURLToPath(new URL('./src/components/Modules', import.meta.url)),
       '@stores': fileURLToPath(new URL('./src/stores', import.meta.url)),
