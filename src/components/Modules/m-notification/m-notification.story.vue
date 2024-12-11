29c25091b5e9976baeeaf18cf5ba441df2288f95
Alexander Schilling <dignityinside@protonmail.com>
2024-12-11 23:39:55 +0100

    Add new stories

---
+++ src/components/Modules/m-notification/m-notification.story.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { reactive } from 'vue';
+import mNotification from '@modules/m-notification/m-notification.vue';
+
+const state = reactive({
+  status: '404',
+  message: 'Not found',
+});
+</script>
+
+<template>
+  <Story title="Modules/Notification">
+    <Variant title="Default">
+      <m-notification :item="state" />
+    </Variant>
+  </Story>
+</template>
---
+++ src/components/Modules/m-spinner/m-spinner.story.vue
@@ -0,0 +1,9 @@
+<script setup>
+import mSpinner from '@modules/m-spinner/m-spinner.vue';
+</script>
+
+<template>
+  <Story title="Modules/Spinner">
+    <m-spinner :isLoading="true" />
+  </Story>
+</template>
